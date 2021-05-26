0c8137ae2d53403d3a2bd6aa7c8cb0aa3e8b8fe7
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-05-26 11:21:29 -0700

    🎨 Make format with black

--- src/ejemplo_api.py
+++ src/ejemplo_api.py
@@ -2,29 +2,33 @@ import os
 import requests
 import json
 
+
 def output(index):
-    per = content['Series'][0]['OBSERVATIONS'][index]['TIME_PERIOD']
-    val = content['Series'][0]['OBSERVATIONS'][index]['OBS_VALUE']
+    per = content["Series"][0]["OBSERVATIONS"][index]["TIME_PERIOD"]
+    val = content["Series"][0]["OBSERVATIONS"][index]["OBS_VALUE"]
     print(per, val)
 
+
 def get_content(url):
-    response= requests.get(url)
-    if response.status_code==200:
+    response = requests.get(url)
+    if response.status_code == 200:
         content = json.loads(response.content)
     return content
 
+
 def desc_grupo(grupo):
-    ocupado_2021 = int(content['Series'][0]['OBSERVATIONS'][0]['OBS_VALUE'])
-    ocupado_2020 = int(content['Series'][0]['OBSERVATIONS'][4]['OBS_VALUE'])
+    ocupado_2021 = int(content["Series"][0]["OBSERVATIONS"][0]["OBS_VALUE"])
+    ocupado_2020 = int(content["Series"][0]["OBSERVATIONS"][4]["OBS_VALUE"])
     salida = f"La población ocupada de {grupo} fue de {ocupado_2021}, cifra menor en {abs(ocupado_2021 - ocupado_2020)} con respecto al mismo trimestre del año anterior."
     print(salida)
+
 
 inegi_token = os.environ["INEGI_TOKEN"]
 indicador = 1002000002
 reciente = "false"
-#Llamado al API
-ocupada_hombres=f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446744/es/0700/false/BIE/2.0/{inegi_token}?type=json"
-ocupada_mujeres=f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446918/es/0700/false/BIE/2.0/{inegi_token}?type=json"
+# Llamado al API
+ocupada_hombres = f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446744/es/0700/false/BIE/2.0/{inegi_token}?type=json"
+ocupada_mujeres = f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446918/es/0700/false/BIE/2.0/{inegi_token}?type=json"
 
 content = get_content(ocupada_mujeres)
 output(0)
