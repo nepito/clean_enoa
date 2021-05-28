02cfe25c8cb9567619b205e4bc34d4a578a18b44
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-05-28 15:21:14 -0700

    ✅ 🏹👾 Hunt some mutants

--- tests/test_moduloA.py
+++ tests/test_moduloA.py
@@ -8,8 +8,32 @@ def test_add_offset():
     obtained = ce.add_offset(a, b)
     assert expected == obtained
 
-content = {"Series":[{"OBSERVATIONS":[{"TIME_PERIOD":1, "OBS_VALUE": "obs1"},{"TIME_PERIOD":2, "OBS_VALUE": "obs2"},{"TIME_PERIOD":3, "OBS_VALUE": "obs3"},{"TIME_PERIOD":4, "OBS_VALUE": "obs4"}]}]}
+
+content = {
+    "Series": [
+        {
+            "OBSERVATIONS": [
+                {"TIME_PERIOD": 1, "OBS_VALUE": 1},
+                {"TIME_PERIOD": 2, "OBS_VALUE": 1},
+                {"TIME_PERIOD": 3, "OBS_VALUE": 1},
+                {"TIME_PERIOD": 4, "OBS_VALUE": 1},
+                {"TIME_PERIOD": 5, "OBS_VALUE": 1},
+            ]
+        }
+    ]
+}
+
+
 def test_output(capsys):  # or use "capfd" for fd-level
     ce.output(content, 1)
     captured = capsys.readouterr()
-    assert captured.out == "2 obs2\n"
+    assert captured.out == "2 1\n"
+
+
+def test_desc_grupo(capsys):
+    ce.desc_grupo(content, "grupo")
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "La población ocupada de grupo fue de 1, cifra menor en 0 con respecto al mismo trimestre del año anterior.\n"
+    )
