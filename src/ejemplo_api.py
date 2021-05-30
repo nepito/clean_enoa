3afc14a34bdee1e47d7dc112a006467cb877fa1f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-05-30 14:45:17 -0700

    👩🏿‍💻 Add the PEA

--- src/ejemplo_api.py
+++ src/ejemplo_api.py
@@ -6,6 +6,7 @@ import clean_enoa as ce
 inegi_token = os.environ["INEGI_TOKEN"]
 ocupada_hombres = f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446744/es/0700/false/BIE/2.0/{inegi_token}?type=json"
 ocupada_mujeres = f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/446918/es/0700/false/BIE/2.0/{inegi_token}?type=json"
+pea = f"https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/289244/es/0700/false/BIE/2.0/{inegi_token}?type=json"
 
 content = ce.get_content(ocupada_mujeres)
 ce.output(content, 0)
@@ -16,3 +17,8 @@ content = ce.get_content(ocupada_hombres)
 ce.output(content, 0)
 ce.output(content, 4)
 ce.desc_grupo(content, "ocupada de hombres")
+
+content = ce.get_content(pea)
+ce.output(content, 0)
+ce.output(content, 4)
+ce.desc_grupo(content, "económicamente activa")
