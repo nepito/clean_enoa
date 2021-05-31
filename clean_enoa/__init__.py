67823c20b2a33793509631e1b71dc9cb2a9bc175
nepito <braulio.rojas@islas.org.mx>
2021-05-30 23:41:44 -0700

    🚾 Apply suggestions from code review
    Co-authored-by: Evaristo Rojas <evaristo.rojas@islas.org.mx>

--- clean_enoa/__init__.py
+++ clean_enoa/__init__.py
@@ -1,4 +1,4 @@
-"""This package make request about unemployment in Mexico to INEGI"""
+"""This package makes requests to INEGI about unemployment in Mexico"""
 
 __version__ = "0.1.0"
 from .moduloA import *  # noqa
